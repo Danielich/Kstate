6e619bba512c5e9de5ec87ca86ca372bc4208b0b
Danniel <122418918+Danielich@users.noreply.github.com>
2024-09-19 14:21:19 -0500

    Update patio.py

--- patio.py
+++ patio.py
@@ -1,9 +1,30 @@
 num = int(input("enter number of row and columns: "))
-rows = []
+rows = [[],[],[],[]]
 columns = [[],[],[],[]]
-for i in range(num):
-    row = input("Enter the style of the row: ")
-    rows.append(row)
-    for i in range(num):
-        columns[i].append(row[i])
 
+for j in range(num):
+    row = input("Enter the style of the row: ")
+    for i in row:
+            rows[j].append(i)
+    for j in range(num):
+        columns[j].append(row[j])
+
+for j in rows:
+    for k in range(num):
+        if j[0] == j[1] and j[0] == j[2]:
+            print(False)
+            break
+        else:
+            j.append(j[0])
+            j.remove(j[0])
+for j in columns:
+    for k in range(num):
+        if j[0] == j[1] and j[0] == j[2]:
+            print(False)
+            break
+        else:
+            j.append(j[0])
+            j.remove(j[0])
+            continue
+    print(True)
+    break
